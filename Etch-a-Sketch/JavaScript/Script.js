2b5ae11d70af6dad5a81248b8680fec568f86d49
SAL13666 <heso482485@gmail.com>
2023-07-11 09:29:02 +0300

    added the sketching functionality

--- Etch-a-Sketch/JavaScript/Script.js
+++ Etch-a-Sketch/JavaScript/Script.js
@@ -6,7 +6,16 @@ for(let i = 0; i < 16 * 16; i++)
     grid.appendChild(document.createElement("div"));
 }
 
-range.addEventListener("change", function() {
+function etch() {
+    document.querySelectorAll(".container div").forEach(function(div) {
+        div.addEventListener("click", function() {
+            div.style.cssText = "background-color: black;";
+        })
+    })    
+}
+
+// need to change it from click to move
+range.addEventListener("click", function() {
     let gridLength = document.querySelector(".range input").value;
     document.querySelector(".range label").textContent = `${gridLength}X${gridLength}`
     document.querySelectorAll(".container div").forEach(function(div) {
@@ -17,6 +26,7 @@ range.addEventListener("change", function() {
         grid.appendChild(document.createElement("div"));
     }
     grid.style.cssText = `grid-template-columns: repeat(${gridLength},1fr); grid-template-rows: repeat(${gridLength},1fr);`
+    etch();
 });
 
-
+etch();
