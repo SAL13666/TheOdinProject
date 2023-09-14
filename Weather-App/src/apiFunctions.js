f2b3c8a3f1cdd4f566dad02639e12dd68bea492b
SAL <65558748+SAL13666@users.noreply.github.com>
2023-09-14 12:37:36 +0200

    added the current day data functionality but the code has bug in the change temp display

--- Weather-App/src/apiFunctions.js
+++ Weather-App/src/apiFunctions.js
@@ -7,6 +7,12 @@ export async function getCurrentWeatherFromApi(location) {
     return data;
 }
 
+export async function getForecastFromApi(location) {
+    let myrequest = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=bb5470923bb74a46afe52441231209&q=${location}`);
+    let data = await myrequest.json();
+    return data;
+}
+
 
 export async function checkIfTheLocationValid(location) {
     let data = await getCurrentWeatherFromApi(location);
--- Weather-App/src/domstuff.js
+++ Weather-App/src/domstuff.js
@@ -1,4 +1,4 @@
-import { getCurrentWeatherFromApi, checkIfTheLocationValid } from "./apiFunctions";
+import { getCurrentWeatherFromApi, checkIfTheLocationValid, getForecastFromApi } from "./apiFunctions";
 
 export async function searchForLocation() {
     let searchField = document.querySelector(".search input");
@@ -11,31 +11,48 @@ export async function searchForLocation() {
         if(await checkIfTheLocationValid(searchFieldValue)) {
             let data = await getCurrentWeatherFromApi(searchFieldValue);
             console.log(data);
-            changeTempDisplay();
-            renderTodayDataToDom(data);
+            changeTempDisplay(data);
+            renderTodayDataToDom(data, searchFieldValue);
         }
-
         searchField.value = "";
     });
 }
 
 
-async function renderTodayDataToDom(data) {
+
+async function renderTodayDataToDom(data, location) {
+    let forecast = await getForecastFromApi(location);
     document.querySelector(".description").innerText = data.current.condition.text;
+    document.querySelector(".Humidity").innerText = `${data.current.humidity} %`;
+    document.querySelector(".wind-speed").innerText = `${data.current.wind_mph} mph`;
+    document.querySelector(".chance-of-rain").innerText = `${forecast.forecast.forecastday[0].day.daily_chance_of_rain} %`;
     document.querySelector(".location").innerText = data.location.name;
+    document.querySelector(".date").innerText = data.location.localtime;
 }
 
 
-export function changeTempDisplay() {
+export function changeTempDisplay(data) {
     document.querySelector(".display").addEventListener("click", () => {
         document.querySelector(".display").classList.toggle("F");
         document.querySelector(".display").classList.toggle("C");
         if(document.querySelector(".display").classList.contains("F")) {
             document.querySelector(".display").innerText = "Display °C";
-
+            displayDataInF(data);
         } else if (document.querySelector(".display").classList.contains("C")) {
             document.querySelector(".display").innerText = "Display °F";
-
+            displayDataInC(data)
         }
     })
+}
+
+
+function displayDataInF(data) {
+    document.querySelector(".dgree").innerText = `${data.current.temp_f} °F`;
+    document.querySelector(".feels-like").innerText = `${data.current.feelslike_f} °F`;
+}
+
+function displayDataInC(data) {
+    // document.querySelector(".dgree").innerText = `${data.current.temp_c} °C`;
+    // document.querySelector(".feels-like").innerText = `${data.current.feelslike_c} °c`;
+    console.log(data);
 }
