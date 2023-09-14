0b9b7bc939eff03abeccecd431d2774fb2fae337
SAL <65558748+SAL13666@users.noreply.github.com>
2023-09-14 17:16:27 +0200

    test if the github live server bug got fixed

--- Weather-App/src/apiFunctions.js
+++ Weather-App/src/apiFunctions.js
@@ -2,13 +2,13 @@
 
 
 export async function getCurrentWeatherFromApi(location) {
-    let myrequest = await fetch(`http://api.weatherapi.com/v1/current.json?key=bb5470923bb74a46afe52441231209&q=${location}`);
+    let myrequest = await fetch(`http://api.weatherapi.com/v1/current.json?key=bb5470923bb74a46afe52441231209&q=${location}`, {mode: 'cors'});
     let data = await myrequest.json();
     return data;
 }
 
 export async function getForecastFromApi(location) {
-    let myrequest = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=bb5470923bb74a46afe52441231209&q=${location}`);
+    let myrequest = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=bb5470923bb74a46afe52441231209&q=${location}`, {mode: 'cors'});
     let data = await myrequest.json();
     return data;
 }
