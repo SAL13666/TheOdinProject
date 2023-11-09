f8a3fd48fe08389cd5a5e6a3a0a884f8b68aaeab
SAL <65558748+SAL13666@users.noreply.github.com>
2023-11-09 18:25:44 +0200

    made the hero section

--- Shopping-Cart/src/components/NavBar.jsx
+++ Shopping-Cart/src/components/NavBar.jsx
@@ -8,9 +8,8 @@ function NavBar({white = false}) {
     let logo = white ? <img className={NavBarCss.logo} src="https://websitedemos.net/brandstore-02/wp-content/uploads/sites/150/2018/12/logo1-free-img-140x47.png" alt="" /> 
     : <img src="https://websitedemos.net/brandstore-02/wp-content/uploads/sites/150/2018/12/favicon-free-img-120x120.png" alt="" />;
     return (
-        <>
+        <header>
             <nav className={NavBarCss.nav}>
-
                 <div className={NavBarCss.navPages}>
                     <Link to="/" className={NavBarCss.link}>
                         {logo}
@@ -22,16 +21,15 @@ function NavBar({white = false}) {
                     <Link to="/About" className={NavBarCss.link}>ABOUT</Link>
                     <Link to="/Contact" className={NavBarCss.link}>CONTACT US</Link>
                     <div className={NavBarCss.cart}>
-                    <p className={NavBarCss.price}>$3000</p>
-                    <div className={NavBarCss.cartContainer}>
-                        <span className={NavBarCss.counter}>3</span>
-                        <FontAwesomeIcon icon={faShoppingBag} className={NavBarCss.bag}/>
-
-                    </div>
+                        <p className={NavBarCss.price}>$3000</p>
+                        <div className={NavBarCss.cartContainer}>
+                            <span className={NavBarCss.counter}>3</span>
+                            <FontAwesomeIcon icon={faShoppingBag} className={NavBarCss.bag}/>
+                        </div>
                     </div>
                 </div>
             </nav>
-        </>
+        </header>
     );
 }
 
--- Shopping-Cart/src/components/pages/Home.jsx
+++ Shopping-Cart/src/components/pages/Home.jsx
@@ -1,8 +1,18 @@
+import { Link } from "react-router-dom";
+import Styles from "../../CSS/HomePage.module.css"
 function Home() {
     return (
-        <div>
-        <h1>Home</h1>  
+        <>
+        <section className={Styles.hero}>
+        <div className={Styles.heroContent}>
+            <h1 className={Styles.h1}>Raining Offers For Hot Summer!</h1>
+            <div className={Styles.buttonContainer}>
+                <h2 className={Styles.h2}>25% Off On All Products</h2>
+                <Link to={"/shop"} className={Styles.a}>SHOP NOW</Link>
+            </div>
         </div>
+        </section>
+        </>
     );
 }
 
