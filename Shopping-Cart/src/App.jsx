0defe7f808d487049970909d973fbaaf23c566bb
SAL <65558748+SAL13666@users.noreply.github.com>
2023-11-13 19:07:05 +0200

    moved the footer to separate container and changed the logic behind the styling of the header

--- Shopping-Cart/src/App.jsx
+++ Shopping-Cart/src/App.jsx
@@ -1,12 +1,14 @@
 import { Outlet } from 'react-router-dom'
 import NavBar from './components/NavBar'
 import './app.css'
+import Footer from './components/Footer'
 function App() {
 
   return (
     <>
       <NavBar white={true}/>
       <Outlet />
+      <Footer/>
     </>
   )
 }
